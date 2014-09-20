2155f89cb282252f878d923f47b1e47cd2fd39ec
Elliot <elliotwhitehead1@yahoo.com>
2014-09-19 22:01:58 -0600

    Repaired ineffecient code

--- Labs/Lab 03/dynamic_size_array_unsorted.cpp
+++ Labs/Lab 03/dynamic_size_array_unsorted.cpp
@@ -37,33 +37,34 @@ bool contains(const int_array& arr, const int& target){
 void resize(int_array& arr){
 	arr.capacity *= 2;
 	int* doubleArray = new int[arr.capacity];
-	for(unsigned int i = 0; i < arr.count; i++){		//copy data to new double-sized array
+	for(unsigned int i = 0; i < arr.count; i++){			//copy data to new double-sized array
 		doubleArray[i] = arr.data[i];
 	}
-	delete [] arr.data;									//delete old dataset and point arr.data to new array
+	delete [] arr.data;										//delete old dataset and point arr.data to new array
 	arr.data = doubleArray;
 }
 
 void add(int_array& arr, const int& payload){
-	if(arr.count == arr.capacity){						//resize array if full
+	if(arr.count == arr.capacity){							//resize array if full
 		resize(arr);
 	}
-	arr.data[arr.count] = payload;						//then add payload
+	arr.data[arr.count] = payload;							//then add payload
 	arr.count++;
 }
 
 bool remove(int_array& arr, const int& target){
-	if(!(contains(arr, target))){						//if target is not contained in data[], return false	
+	if(!(contains(arr, target))){							//if target is not contained in data[], return false	
 		return false;
 	}
 	else{
-		for(unsigned int i = 0; i < arr.count; i++){	//otherwise, find it!
+		if(i == (arr.count - 1)){							//if target is the last of the array, simply decrement count
+			arr.count--;
+			return true;
+		}
+		
+		for(unsigned int i = 0; i < arr.count; i++){		//otherwise, find it!
 			if(arr.data[i] == target){
-				if(i == (arr.count - 1)){				//if target is the last of the 
-					arr.count--;
-					return true;
-				}
-				arr.data[i] = arr.data[(arr.count -1)];
+				arr.data[i] = arr.data[(arr.count -1)];		//replace target with last item
 				arr.count--;
 				return true;
 			}

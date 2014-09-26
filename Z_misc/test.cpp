2b55d91cba341432945d992e19bedd7c3f3c155d
Elliot <elliotwhitehead1@yahoo.com>
2014-09-26 14:44:46 -0600

    pracitce code

---
+++ Z_misc/test.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+using namespace std;
+
+int arr[] = new int[20];
+
+arr[0] = 1;
+arr[1] = 2;
+arr[2] = 3;
+arr[3] = 4;
+arr[4] = 5;
+arr[5] = 6;
+arr[6] = 7;
+arr[7] = 8;
+
+
+for (unsigned int = 7; i >=0; --i){
+	cout << arr[i];
+}
+
+
+//MEH

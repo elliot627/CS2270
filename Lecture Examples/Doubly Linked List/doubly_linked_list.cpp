3ba5dbd25a53205acdf6bf141062832677c5e533
Elliot Whitehead <elliotwhitehead1@yahoo.com>
2014-10-01 15:37:44 -0600

    added lecture code from class

--- Lecture Examples/Doubly Linked List/doubly_linked_list.cpp
+++ Lecture Examples/Doubly Linked List/doubly_linked_list.cpp
@@ -2,8 +2,7 @@
 
 using namespace std;
 
-void add_node(node*& head_ptr, node*& tail_ptr, const int& payload)
-{
+void add_node(node*& head_ptr, node*& tail_ptr, const int& payload){
 	node* added_node = new node;
 	added_node->data = payload;
 	if (head_ptr == nullptr)											//if list is empty, add first node and assign pointers
@@ -44,14 +43,13 @@ void add_node(node*& head_ptr, node*& tail_ptr, const int& payload)
 	}
 }
 
-bool remove_node(node*& head_ptr, node*& tail_ptr, const int& target)
-{
+bool remove_node(node*& head_ptr, node*& tail_ptr, const int& target){
 	if (head_ptr == nullptr) return false;
 
 	node* removed_node_ptr = head_ptr;
-	if (removed_node_ptr->data == target)
-	{
+	if (removed_node_ptr->data == target){
 		head_ptr = head_ptr->next;
+		head_ptr->prev = nullptr;
 		delete removed_node_ptr;
 		removed_node_ptr = nullptr;
 		return true;
@@ -62,20 +60,23 @@ bool remove_node(node*& head_ptr, node*& tail_ptr, const int& target)
 		removed_node_ptr->next->data != target)
 		removed_node_ptr = removed_node_ptr->next;
 
-	if (removed_node_ptr != nullptr &&
-		removed_node_ptr->next != nullptr)
-	{
+	if (removed_node_ptr != nullptr && removed_node_ptr->next != nullptr){
 		node* next_node_ptr = removed_node_ptr->next->next;
 		delete removed_node_ptr->next;
 		removed_node_ptr->next = next_node_ptr;
+		if(next_node_ptr != nullptr){
+			next_node_ptr->prev = removed_node_ptr;
+		}
+		else{
+			tail_ptr = removed_node_ptr;
+		}
 		return true;
 	}
 
 	return false;
 }
 
-bool find_list(const node* head_ptr, const int& target)
-{
+bool find_list(const node* head_ptr, const int& target){
 		const node* cursor = head_ptr;
 	while (cursor != nullptr)
 	{
@@ -86,8 +87,7 @@ bool find_list(const node* head_ptr, const int& target)
 	return false;
 }
 
-void clear_list(node*& head_ptr, node*& tail_ptr)
-{
+void clear_list(node*& head_ptr, node*& tail_ptr){
 	node* destroyed_node_ptr;
 	while (head_ptr != nullptr)
 	{
@@ -95,11 +95,10 @@ void clear_list(node*& head_ptr, node*& tail_ptr)
 		head_ptr = head_ptr->next;
 		delete destroyed_node_ptr;
 	}
-
+	tail_ptr = nullptr;
 }
 
-void print_list(const node* head_ptr)
-{
+void print_list(const node* head_ptr){
 	const node* cursor = head_ptr;
 	while (cursor != nullptr)
 	{
@@ -109,20 +108,24 @@ void print_list(const node* head_ptr)
 	cout << endl;
 }
 
-void print_list_backwards(const node* tail_ptr)
-{
-
+void print_list_backwards(const node* tail_ptr){
+	cont node* cursor = tail_ptr;
+	while(cursor != nullptr){
+		cout << cursor->data << " ";
+		cursor = cursor->prev;
+	}
 }
 
 void copy_list(const node* source_head_ptr,
 	node*& dest_head_ptr, node*& dest_tail_ptr)
 {
 	clear_list(dest_head_ptr, dest_tail_ptr);
-	if (source_head_ptr == nullptr)
+	if (source_head_ptr == nullptr)												//check if list to be copied is empty
 		return;
 	dest_head_ptr = new node();
 	dest_head_ptr->data = source_head_ptr->data;
 	dest_head_ptr->next = nullptr;
+	dest_head_ptr-prev = nullptr;
 	node* cursor = dest_head_ptr;
 	while (source_head_ptr->next != nullptr)
 	{
@@ -130,6 +133,8 @@ void copy_list(const node* source_head_ptr,
 		cursor->next = new node();
 		cursor->next->data = source_head_ptr->data;
 		cursor->next->next = nullptr;
+		cursor->next->prev = cursor;
 		cursor = cursor->next;
+		dest_tail_ptr = cursor;
 	}
 }

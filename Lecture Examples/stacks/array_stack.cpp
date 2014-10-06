51a9691db5f729c7083ad4e0677a7de6504ec58e
Elliot Whitehead <elliotwhitehead1@yahoo.com>
2014-10-06 15:35:58 -0600

    Added lecture code

--- Lecture Examples/stacks/array_stack.cpp
+++ Lecture Examples/stacks/array_stack.cpp
@@ -18,17 +18,23 @@ void print(const array_stack& stk){
 }
 
 void push(array_stack& stk, const int& payload){
-
+	add(stk.arr, payload);
 }
 
 bool pop(array_stack& stk){
-
+	if(stk.arr.count == 0){
+		return false;
+	}
+	stk.arr.count--;
+	return true;
 }
 
 int top(const array_stack& stk){
-
+	if(stk.arr.count != 0){
+		return stk.arr[stk.arr.count - 1];
+	}
 }
 
 bool empty(const array_stack& stk){
-
+	return stk.arr.count == 0;
 }

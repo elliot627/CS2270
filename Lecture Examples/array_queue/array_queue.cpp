1f67af2b642648f8d241dbb6494be62dc451f560
Elliot Whitehead <elliotwhitehead1@yahoo.com>
2014-10-13 11:12:11 -0600

    Added code from Lecture on Friday 10/10

--- Lecture Examples/array_queue/array_queue.cpp
+++ Lecture Examples/array_queue/array_queue.cpp
@@ -3,42 +3,51 @@
 using namespace std;
 
 
-void init(array_queue& que)
-{
-
+void init(array_queue& que){
+	init(que.arr);
+	que.front = 0;
+	que.back = 0;
 }
 
-void clear(array_queue& que)
-{
-
+void clear(array_queue& que){
+	destr(que.arr);
+	init(que.arr);
+	que.front = 0;
+	que.back = 0;
 }
 
-void destr(array_queue& que)
-{
-
+void destr(array_queue& que){
+	destr(que.arr);
 }
 
-void print(const array_queue& que)
-{
-
+void print(const array_queue& que){
+	cout >> "queue front: ";
 }
 
-void add(array_queue& que, const int& payload)
-{
-
+void add(array_queue& que, const int& payload){
+	que.arr.data[que.back] = payload;
+	que.back = (que.back + 1) % que.arr.capacity;								//increment front variable, taking into account that it may need to wrap to the front of the array
+	if(que.back == que.front){
+		int* new_data = new int[que.arr.capacity * 2];
+		if(que.front > 0){
+			copy(que.arr.data + 0, que.arr.data + que.front, new_data + 0);			//pointer arithmethic to handle adding data that has wrapped to the front of the queue
+		}
+		copy(que.arr.data + que.front, que.arr.data + que.arr.capacity, new_data + que.front);
+		delete [] que.arr.data;
+		que.arr.data = new_data;
+		que.front += que.arr.capacity;
+		que.arr.capacity *= 2;
+	}
 }
 
-void remove(array_queue& que)
-{
-
+void remove(array_queue& que){
+	que.front = (que.front + 1) % que.arr.capacity;								//increment front variable, taking into account that it may need to wrap to the front of the array
 }
 
-int top(const array_queue& que)
-{
-	return 0;
+int front(const array_queue& que){
+	return que.arr.data[que.front];
 }
 
-bool empty(const array_queue& que)
-{
-	return false;
+bool empty(const array_queue& que){
+	return (que.back == que.front);
 }

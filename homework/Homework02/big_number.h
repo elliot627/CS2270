3ce4ecd16d98b86136f204fb21369eaafa238237
Elliot Whitehead <elliotwhitehead1@yahoo.com>
2014-10-16 09:32:55 -0600

    Added Homework 02 files

---
+++ homework/Homework02/big_number.h
@@ -0,0 +1,67 @@
+#include "doubly_linked_list.h"
+
+using namespace std;
+
+class big_number 
+{
+	public:
+	
+		// constructors (work like init() methods)
+		big_number();
+		big_number(int i);
+		big_number(const big_number& m);
+		big_number(const string& s, unsigned int base);
+		
+		// assignment operator
+		// big_number a = 98; big_number b = 0; ... b = a;
+		big_number& operator=(const big_number& m);
+		
+		// destructor (works like destr() method)
+		~big_number();
+	
+		// self-assigning arithmetic operators
+		big_number& operator+=(const big_number& addend);
+		big_number& operator-=(const big_number& subtractand);
+		big_number& operator*=(const big_number& multiplicand);
+		big_number& operator/=(const big_number& divisor);
+	
+		// positive modulus
+		big_number& operator%=(const big_number& divisor);
+		
+		// overload prefix increment
+		big_number& operator++();  
+		// overload prefix decrement
+		big_number& operator--();  
+		
+		// input and output operators
+		friend std::ostream& operator<<(std::ostream& os, const big_number& bignum);
+		friend std::istream& operator>>(std::istream& is, big_number& bignum);
+		
+		// arithmetic operators
+		friend big_number operator+(const big_number& a, const big_number& b);
+		friend big_number operator-(const big_number& a, const big_number& b);
+		friend big_number operator*(const big_number& a, const big_number& b);
+		friend big_number operator/(const big_number& a, const big_number& b);
+		friend big_number operator%(const big_number& a, const big_number& b);
+		// hard to do without fast multiplication
+		friend big_number factorial(const big_number& a);
+
+		// comparison operators
+		friend bool operator>(const big_number& a, const big_number& b);
+		friend bool operator>=(const big_number& a, const big_number& b);
+		friend bool operator<(const big_number& a, const big_number& b);
+		friend bool operator<=(const big_number& a, const big_number& b);	
+		friend bool operator==(const big_number& a, const big_number& b);
+		friend bool operator!=(const big_number& a, const big_number& b);
+
+	private:
+	
+		node* head_ptr;
+		node* tail_ptr;
+		unsigned int digits;
+		bool positive;
+		unsigned int base;
+		
+		// helper functions can go here
+		
+};
---
+++ homework/Homework02/test_big_number.cpp
@@ -0,0 +1,90 @@
+#include "big_number.h"
+
+using namespace std;
+
+int main()
+{
+	int n1, n2;
+	unsigned int base;
+	string s;
+			
+	char choice;
+	do
+	{
+		cout << "Type 'd' to test default constructor" << endl;
+		cout << "Type 'i' to test int constructor" << endl;
+		cout << "Type 's' to test string constructor" << endl;
+		cout << "Type 'a' to test assignment" << endl;
+		cout << "Type '>' to test input operator" << endl;
+		cout << "Type '=' to test comparison operators" << endl;
+		cout << "Type 'q' to quit" << endl;
+		cin >> choice;
+	
+		if (toupper(choice) == 'D')
+		{
+			big_number a;
+			cout << "Default constructor gives " << a << endl;
+		}
+		
+		if (toupper(choice) == 'I')
+		{
+			cout << "Enter an integer: ";
+			cin >> n1;
+			big_number b(n1);
+			cout << "Int constructor with " << n1 << " gives " << b << endl;
+		}
+		
+		if (toupper(choice) == 'S')
+		{
+			cout << "Enter a base: ";
+			cin >> base;
+			cout << "Enter a string: ";
+			cin >> s;
+			big_number e(s, base);
+			cout << "String constructor with " << s << " in base " << 
+				base << " gives " << e << endl;
+		}
+		
+		if (toupper(choice) == 'A')
+		{
+			big_number e(9);
+			big_number d;
+			d = e;
+			cout << "Assignment of " << e << " to ";
+			++e;
+			cout << d << " gives " << e << endl;
+			if (d == e) cout << "boo, shallow copy";
+		}
+		
+		if (choice == '>')
+		{
+			big_number n1;
+			cout << "Enter a big number: ";
+			cin >> n1;
+			cout << "You entered " << n1 << endl;
+		}
+		
+		if (choice == '=')
+		{
+			cout << "Enter a number: ";
+			cin >> n1;
+			cout << "Enter another number: ";
+			cin >> n2;
+			big_number f(n1);
+			big_number g(n2);
+			if (f == g)
+				cout << f << " == " << g << endl;
+			if (f != g)
+				cout << f << " != " << g << endl;
+			if (f < g)
+				cout << f << " < " << g << endl;
+			if (f <= g)
+				cout << f << " <= " << g << endl;
+			if (f > g)
+				cout << f << " > " << g << endl;
+			if (f >= g)
+				cout << f << " >= " << g << endl;
+		}
+
+	} while (toupper(choice) != 'Q'); 
+}

4601cdd41584e367d223d91cabafabf62e348fb5
Elliot <elliotwhitehead1@yahoo.com>
2014-10-23 15:18:02 -0600

    Finished Midterm 02

--- Labs/Midterm 02/question13.cpp
+++ Labs/Midterm 02/question13.cpp
@@ -1,9 +1,45 @@
 #include "array_stack.h"
+#include <string>																			//include string for use of member functions, if needed
 
 using namespace std;
 
 bool balanced_brackets(const string&s){
+	array_stack arr_stack;																	//declare,
+	init(arr_stack);																		//and initialize stack for brackets
 	
+	for(unsigned int i = 0; i < s.size(); i++){												//loop through given string, and 
+		if(s[i] == '{' || s[i] == '(' || s[i] == '['){										//push opening  brackets onto the stack.
+			push(arr_stack, s[i]);
+		}
+		
+		if(s[i] == '}'){																	//if a closing bracket is found,
+			if(top(arr_stack) == '{'){														//check if it matches the bracket at the top of the stack
+				pop(arr_stack);																//if so, pop it's match from the stack
+			}
+			else{
+				return false;																//if they don't match, return false, as this means that the brackets are out of order
+			}
+		}
+		
+		if(s[i] == ')'){																	//if a closing bracket is found,
+			if(top(arr_stack) == '('){														//check if it matches the bracket at the top of the stack
+				pop(arr_stack);																//if so, pop it's match from the stack
+			}
+			else{
+				return false;																//if they don't match, return false, as this means that the brackets are out of order
+			}
+		}
+		
+		if(s[i] == ']'){																	//if a closing bracket is found,
+			if(top(arr_stack) == '['){														//check if it matches the bracket at the top of the stack
+				pop(arr_stack);																//if so, pop it's match from the stack
+			}
+			else{
+				return false;																//if they don't match, return false, as this means that the brackets are out of order
+			}
+		}
+	}
+	return (empty(arr_stack));																//return whether all three stacks are empty - If all are empty, the string of brackets is balanced!
 }
 
 int main()
@@ -24,9 +60,9 @@ int main()
 			if (test[0] == 'q' || test[0] == 'Q')
 				exit(0);
 			if (balanced_brackets(test))
-				cout << test << " has balanced brackets" << endl;
+				cout << test << " is BALANCED!" << endl;
 			else
-				cout << test << " has unbalanced brackets" << endl;
+				cout << test << " is NOT balanced" << endl;
 		}
 	}
 }

c3552f1a3be8792913fb12f5e7000b0f08fbf955
Elliot Whitehead <elliotwhitehead1@yahoo.com>
2014-10-30 00:00:56 -0600

    Made testing file much simpler thanks to they shiny new oStream operator

--- Homework/Homework02/test_big_number.cpp
+++ Homework/Homework02/test_big_number.cpp
@@ -4,100 +4,58 @@ using namespace std;
 
 int main(){
 
-  cout << "\t\t Testing int constructor..." << endl;
+  cout << "\t\t Testing int constructor...\n" << endl;
   big_number intTest(123456789);
-  print_list(intTest.getHead());
-  cout << "head_ptr points to: " << intTest.getHeadData() << endl;
-  cout << "tail_ptr points to: " << intTest.getTailData() << endl;
-  cout << "This has " << intTest.getDigits() << " digits" << endl;
-  cout << "Positive: " << intTest.getPositive() << "\n" << endl;
+  cout << intTest;
 
 
   big_number intTest2(987654321);
-  print_list(intTest2.getHead());
-  cout << "head_ptr points to: " << intTest2.getHeadData() << endl;
-  cout << "tail_ptr points to: " << intTest2.getTailData() << endl;
-  cout << "This has " << intTest2.getDigits() << " digits" << endl;
-  cout << "Positive: " << intTest2.getPositive() << "\n" << endl;
+  cout << intTest2;
 
-  cout << "\t\t Testing int constructor with single digit..." << endl;
+
+  cout << "\t\t Testing int constructor with single digit...\n" << endl;
   big_number intTest3(3);
-  print_list(intTest3.getHead());
-  cout << "head_ptr points to: " << intTest3.getHeadData() << endl;
-  cout << "tail_ptr points to: " << intTest3.getTailData() << endl;
-  cout << "This has " << intTest3.getDigits() << " digits" << endl;
-  cout << "Positive: " << intTest3.getPositive() << "\n" << endl;
+  cout << intTest3;
 
 
-  cout << "\t\t Testing int constructor with single negative digit..." << endl;
+  cout << "\t\t Testing int constructor with single negative digit...\n" << endl;
   big_number intTest4(-3);
-  print_list(intTest4.getHead());
-  cout << "head_ptr points to: " << intTest4.getHeadData() << endl;
-  cout << "tail_ptr points to: " << intTest4.getTailData() << endl;
-  cout << "This has " << intTest4.getDigits() << " digits" << endl;
-  cout << "Positive: " << intTest4.getPositive() << "\n" << endl;
+  cout << intTest4;
 
 
-  cout << "\t\t Testing int constructor with negative int..." << endl;
+  cout << "\t\t Testing int constructor with negative int...\n" << endl;
   big_number intTest5(-305);
-  print_list(intTest5.getHead());
-  cout << "head_ptr points to: " << intTest5.getHeadData() << endl;
-  cout << "tail_ptr points to: " << intTest5.getTailData() << endl;
-  cout << "This has " << intTest5.getDigits() << " digits" << endl;
-  cout << "Positive: " << intTest5.getPositive() << "\n" << endl;
+  cout << intTest5;
 
 
-  cout << "\t\t Testing default constructor..." << endl;
+  cout << "\t\t Testing default constructor...\n" << endl;
   big_number defaultTest;
-  print_list(defaultTest.getHead());
-  cout << "head_ptr points to: " << defaultTest.getHeadData() << endl;
-  cout << "tail_ptr points to: " << defaultTest.getTailData() << endl;
-  cout << "This has " << defaultTest.getDigits() << " digits" << endl;
-  cout << "Positive: " << defaultTest.getPositive() << "\n" << endl;
+  cout << defaultTest;
 
-  cout << "\t\t Testing copy constructor..." << endl;
+
+  cout << "\t\t Testing copy constructor...\n" << endl;
   big_number copyTest(intTest);
-  print_list(copyTest.getHead());
-  cout << "head_ptr points to: " << copyTest.getHeadData() << endl;
-  cout << "tail_ptr points to: " << copyTest.getTailData() << endl;
-  cout << "This has " << copyTest.getDigits() << " digits" << endl;
-  cout << "Positive: " << copyTest.getPositive() << "\n" << endl;
+  cout << copyTest;
 
 
-  cout << "\t\t Testing string constructor..." << endl;
+  cout << "\t\t Testing string constructor...\n" << endl;
   big_number strTest("123456789", 10);
-  print_list(strTest.getHead());
-  cout << "head_ptr points to: " << strTest.getHeadData() << endl;
-  cout << "tail_ptr points to: " << strTest.getTailData() << endl;
-  cout << "This has " << strTest.getDigits() << " digits" << endl;
-  cout << "Positive: " << strTest.getPositive() << "\n" <<  endl;
+  cout << strTest;
 
 
-  cout << "\t\t Testing string constructor with single digit..." << endl;
-  big_number strTest3("8", 10);
-  print_list(strTest3.getHead());
-  cout << "head_ptr points to: " << strTest3.getHeadData() << endl;
-  cout << "tail_ptr points to: " << strTest3.getTailData() << endl;
-  cout << "This has " << strTest3.getDigits() << " digits" << endl;
-  cout << "Positive: " << strTest3.getPositive() << "\n" <<  endl;
+  cout << "\t\t Testing string constructor with single digit...\n" << endl;
+  big_number strTest2("8", 10);
+  cout << strTest2;
 
 
-  cout << "\t\t Testing string constructor with negative number..." << endl;
-  big_number strTest2("-98765432101234567893", 10);
-  print_list(strTest2.getHead());
-  cout << "head_ptr points to: " << strTest2.getHeadData() << endl;
-  cout << "tail_ptr points to: " << strTest2.getTailData() << endl;
-  cout << "This has " << strTest2.getDigits() << " digits" << endl;
-  cout << "Positive: " << strTest2.getPositive() << "\n" << endl;
+  cout << "\t\t Testing string constructor with negative number...\n" << endl;
+  big_number strTest3("-98765432101234567893", 10);
+  cout << strTest3;
 
 
-  cout << "\t\t Testing string constructor with single negative digit.." << endl;
+  cout << "\t\t Testing string constructor with single negative digit..\n" << endl;
   big_number strTest4("-8", 10);
-  print_list(strTest4.getHead());
-  cout << "head_ptr points to: " << strTest4.getHeadData() << endl;
-  cout << "tail_ptr points to: " << strTest4.getTailData() << endl;
-  cout << "This has " << strTest4.getDigits() << " digits" << endl;
-  cout << "Positive: " << strTest4.getPositive() << "\n" << endl;
+  cout << strTest4;
 
 
 	//~ int n1, n2;

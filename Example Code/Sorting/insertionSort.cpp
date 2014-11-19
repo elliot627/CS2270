a9edfb0afc38f1d56fe2af0ff548b1e7faf2290b
Elliot Whitehead <elliotwhitehead1@yahoo.com>
2014-11-19 15:06:09 -0700

    Started implementing my insertion sort method

---
+++ Example Code/Sorting/insertionSort.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+#inlcued <string>
+
+using namespace std;
+template <class ItemType>
+
+void insertionSort(ItemType* arr, unsigned int size){
+  for(unsigned int unsorted = 1;
+}

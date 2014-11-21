b62e67a3400dfe8afc5c9525b2576af8ffc836ba
Elliot Whitehead <elliotwhitehead1@yahoo.com>
2014-11-20 23:19:05 -0700

    Added pattern's executable to gitignore

--- Labs/Lab 05/pattern.cpp
+++ Labs/Lab 05/pattern.cpp
@@ -9,17 +9,26 @@ using namespace std;
 void pattern(ostream& outs, unsigned int n, unsigned int i);
 
 int main(){
-  int i2;
-  cout << "Enter an integer (a power of 2): ";
-  cin >> i2;
-  pattern(cout, i2, 0);
+  int i2 = 1;
+  while(i2 != 0){
+    cout << "Enter an integer (a power of 2): \n(use 0 to quit): ";
+    cin >> i2;
+    pattern(cout, i2, 0);
+  }
   return (EXIT_SUCCESS);
 }
 
 // i is the offset
 // n is the asterisks
 void pattern(ostream& outs, unsigned int n, unsigned int i){
-  if(n == 0){
+  if(n == 1){
+    for(unsigned int k = 0; k < i ; k++){
+      outs << "  ";
+    }
+    for(unsigned int k = 0; k < n; k++){
+      outs << "* ";
+    }
+    outs << "\n";
     return;
   }
   pattern(outs, (n/2), i);
@@ -31,6 +40,6 @@ void pattern(ostream& outs, unsigned int n, unsigned int i){
   }
   outs << "\n";
   pattern(outs, (n/2), ((n/2) + i) );
-  
+
 }
 

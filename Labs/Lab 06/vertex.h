bf90dbfc60ef1ca8be89d85a88a4e9e7254f388e
Elliot <elliotwhitehead1@yahoo.com>
2014-12-04 14:13:03 -0700

    Fixed indentation of vertex.h

--- Labs/Lab 06/vertex.h
+++ Labs/Lab 06/vertex.h
@@ -6,45 +6,42 @@
 
 using namespace std;
 
-class vertex
-{
+class vertex{
+	
     public:
-	vertex(const string& initname,
-	       int init_latitude_degrees,
-	       int init_latitude_minutes,
-	       int init_longitude_degrees,
-	       int init_longitude_minutes)
-	    {
-		name_of_city = initname;
-		latitude_degrees = init_latitude_degrees;
-		longitude_degrees = init_longitude_degrees;
-		latitude_minutes = init_latitude_minutes;
-		longitude_minutes = init_longitude_minutes;
-	    }
-	int get_latitude_degrees() const
-	    {
+	vertex(const string& initname, int init_latitude_degrees, int init_latitude_minutes, int init_longitude_degrees, int init_longitude_minutes){
+					name_of_city = initname;
+					latitude_degrees = init_latitude_degrees;
+					longitude_degrees = init_longitude_degrees;
+					latitude_minutes = init_latitude_minutes;
+					longitude_minutes = init_longitude_minutes;
+	}
+	
+	int get_latitude_degrees() const{
 		return latitude_degrees;
-	    }
-	int get_latitude_minutes() const
-	    {
+	}
+	
+	int get_latitude_minutes() const {
 		return latitude_minutes;
-	    }
-	int get_longitude_degrees() const
-	    {
+	}
+	
+	int get_longitude_degrees() const{
 		return longitude_degrees;
-	    }
-	int get_longitude_minutes() const
-	    {
+	}
+	
+	int get_longitude_minutes() const{
 		return longitude_minutes;
-	    }
-	string get_city_name() const
-	    {
+	}
+	
+	string get_city_name() const{
 		return name_of_city;
-	    }
-	bool operator <(const vertex& anothervertex) const
-	    {
+	}
+	
+	bool operator <(const vertex& anothervertex) const{
 		return !(get_city_name() == anothervertex.get_city_name());
-	    }
+	}
+  
+  
     private:
 	string name_of_city;
 	int latitude_degrees;

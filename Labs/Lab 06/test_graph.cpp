32d29db65f3901ba928325450d4c96c3f4382610
Elliot <elliotwhitehead1@yahoo.com>
2014-12-05 00:40:02 -0700

    actually fixed indentation

--- Labs/Lab 06/test_graph.cpp
+++ Labs/Lab 06/test_graph.cpp
@@ -4,6 +4,6 @@ using namespace std;
 
 int main(){
 	graph g;
-    double limit = 2000;
-    init_graph_from_file(g, "world_cities.txt", limit);
+	double limit = 2000;
+	init_graph_from_file(g, "world_cities.txt", limit);
 }
